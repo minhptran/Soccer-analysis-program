52e85fd3513a8ce13b1abe4f83a6e557f8fef429
tranmi60 <minhphuoc.tran@mail.utoronto.ca>
2025-01-19 22:53:19 -0500

    forgot to save changes

--- team_assigner/team_assigner.py
+++ team_assigner/team_assigner.py
@@ -3,8 +3,7 @@ from sklearn.cluster import KMeans
 class TeamAssigner:
     def __init__(self):
         self.team_colors = {}
-        self.player_team_dict = {} 
-
+        self.player_team_dict = {}
     
     def get_clustering_model(self,image):
         # Reshape the image to 2D array
@@ -15,12 +14,16 @@ class TeamAssigner:
         kmeans.fit(image_2d)
 
         return kmeans
-    def get_player_color(self, frame, bbox):
+
+    def get_player_color(self,frame,bbox):
         image = frame[int(bbox[1]):int(bbox[3]),int(bbox[0]):int(bbox[2])]
 
-        top_half_image = image[0: int(image.shape[0]/2), :]
+        top_half_image = image[0:int(image.shape[0]/2),:]
 
+        # Get Clustering model
         kmeans = self.get_clustering_model(top_half_image)
+
+        # Get the cluster labels forr each pixel
         labels = kmeans.labels_
 
         # Reshape the labels to the image shape
@@ -35,29 +38,36 @@ class TeamAssigner:
 
         return player_color
 
-    def assign_team_color(self, frame, player_detections):
+
+    def assign_team_color(self,frame, player_detections):
+        
         player_colors = []
         for _, player_detection in player_detections.items():
-            bbox = player_detection['bbox']
-            player_color = self.get_player_color(frame, bbox)
+            bbox = player_detection["bbox"]
+            player_color =  self.get_player_color(frame,bbox)
             player_colors.append(player_color)
         
-        kmeans = kmeans(n_clusters=2, init="k-means++", n_init=1)
+        kmeans = KMeans(n_clusters=2, init="k-means++",n_init=10)
         kmeans.fit(player_colors)
 
         self.kmeans = kmeans
-        self.team_colors[1] = kmeans.cluster_centers+[0]
+
+        self.team_colors[1] = kmeans.cluster_centers_[0]
         self.team_colors[2] = kmeans.cluster_centers_[1]
 
 
-    def get_player_team(self, frame, player_bbox, player_id):
+    def get_player_team(self,frame,player_bbox,player_id):
         if player_id in self.player_team_dict:
             return self.player_team_dict[player_id]
-        
-        player_color = self.get_player_color(frame, player_bbox)
+
+        player_color = self.get_player_color(frame,player_bbox)
 
         team_id = self.kmeans.predict(player_color.reshape(1,-1))[0]
-        team_id +=1
+        team_id+=1
+
+        if player_id ==91:
+            team_id=1
+
         self.player_team_dict[player_id] = team_id
 
         return team_id
